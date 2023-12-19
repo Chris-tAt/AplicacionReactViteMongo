918da34be899c9fea88edababcdda844503dc7d1
Chris_tAt <christati1205@gmail.com>
2023-12-18 19:53:55 -0500

    se crearon algunas modificaciones para que que pudieran levantarse las rutas de register y login, en el back se creo otra ruta de verificacion de usuario

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,5 @@
 import {Router} from 'express'
-import {register, login, logout, profile} from '../controllers/auth.controller.js'
+import {register, login, logout, profile, verifyToken} from '../controllers/auth.controller.js'
 import { authRequire } from '../middlewares/validateToken.js'
 import {validateSchema} from '../middlewares/validator.middlewares.js'
 import { registerSchema, loginSchema } from "../schemas/auth.schema.js";
@@ -10,5 +10,6 @@ router.post('/register', validateSchema(registerSchema), register)
 router.post('/login', validateSchema(loginSchema), login)
 router.post('/logout', logout)
 router.get('/profile', authRequire, profile)
+router.get("/verify", verifyToken)
 
 export default router
