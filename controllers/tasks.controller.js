03fa93013dc999b76fde4e443ab059d8cd4cd40e
Chris_tAt <christati1205@gmail.com>
2023-12-20 21:20:12 -0500

    se añadieron algunas rutas y se modificaron algunas cosas que eran necesarias para el funcionamiento de las fechas y de la ruta editar y borrar tarea

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,16 +1,21 @@
 import Task from "../model/tasks.model.js";
 
 export const getTasks = async (req, res) => {
-  const tasks = await Task.find(
-    {
-      user: req.decoded.id
-    }
-  ).populate('user');
-  res.json(tasks);
+  try {
+    const tasks = await Task.find(
+      {
+        user: req.decoded.id
+      }
+    ).populate('user');
+    res.json(tasks);
+  } catch (error) {
+    return res.status(500).json({message: "task not found"})
+  }
 };
 
 export const createTask = async (req, res) => {
-  const { title, description, date } = req.body;
+  try {
+    const { title, description, date } = req.body;
 console.log(req.decoded)
 
   const newTasks = new Task({
@@ -22,24 +27,39 @@ console.log(req.decoded)
 
   const saveTasks = await newTasks.save();
   res.json(saveTasks);
+  } catch (error) {
+    return res.status(500).json({message: "task not found"})
+  }
 };
 
 export const deleteTask = async (req, res) => {
-  const task = await Task.findByIdAndDelete(req.params.id);
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
   if (!task) return res.status(404).json({ message: "Task not found" });
   return res.sendStatus(204)
+  } catch (error) {
+    return res.status(404).json({message: "task not found"})
+  }
 };
 
 export const getTask = async (req, res) => {
+ try {
   const task = await Task.findById(req.params.id);
   if (!task) return res.status(404).json({ message: "Task not found" });
   res.json(task);
+ } catch (error) {
+  return res.status(404).json({message: "task not found"})
+ }
 };
 
 export const updateTask = async (req, res) => {
-  const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
+ try {
+   const task = await Task.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
   if (!task) return res.status(404).json({ message: "Task not found" });
   res.json(task);
+ } catch (error) {
+  return res.status(404).json({message: "task not found"})
+ }
 };
